99fcbe977209ce12d4e3b7a140ce8a080d219bc3
01Sanjana <sanjanaadmane926@gmail.com>
2026-08-19 15:43:05 +0530

    Improved version turn system

--- part1.py
+++ part1.py
@@ -7,29 +7,37 @@
 
 
 # numbers = ["X", "O", "3", "4", "X", "6", "O", "8", "X"]
-numbers=[]
-for i in range(1,10):
+numbers = []
+
+# Create the board
+for i in range(1, 10):
     numbers.append(i)
 
 
-for i in range(0,9,3):
+# Display the board
+for i in range(0, 9, 3):
     print(f"{numbers[i]} | {numbers[i+1]} | {numbers[i+2]}")
     print("----------")
 
-i=0
-while(i<3):
-    user1=int(input("user1 ,Enter the position (1-9) :"))
-    user2=int(input("user2 ,Enter the position (1-9) :"))
-    i+=1
 
-    numbers[user1-1]='X'
-    if user1==user2:
-        print("Invalid !")
-    else:
-        numbers[user2-1]='O'
+player = "X"
 
-    print("\nAFTER\n")
-    print(numbers)
-    for num in range(0,9,3):
-        print(f"{numbers[num]} | {numbers[num+1]} | {numbers[num+2]}")
+# Take turns
+for turn in range(6):
+
+    position = int(input(f"Player {player}, enter the position (1-9): "))
+
+    numbers[position - 1] = player
+
+    # Display updated board
+    print()
+
+    for i in range(0, 9, 3):
+        print(f"{numbers[i]} | {numbers[i+1]} | {numbers[i+2]}")
         print("----------")
+
+    # Switch player
+    if player == "X":
+        player = "O"
+    else:
+        player = "X"
